f729151bd0db4d84190c5178861e0023bf768d5f
@pauloddr <git@pauloddr.com>
2018-09-18 19:37:14 +0000

    travis consistency adjustments

--- test/DatastoreAdapter.js
+++ test/DatastoreAdapter.js
@@ -4,6 +4,8 @@ const DatastoreAdapter = require('../')
 const {behaves} = require('@gfa/core/test/behaviors/DatabaseAdapter')
 
 describe('DatastoreAdapter', function () {
+  this.timeout(5000)
+
   before(function () {
     this.adapter = new DatastoreAdapter()
   })
